67f43011a877933c0de309b659b0afe3c87c9491
Chris <cocogemayel2006@gmail.com>
2022-12-04 17:06:28 +0100

    first script

---
+++ JavaScript.js
@@ -0,0 +1,46 @@
+var leftboarderwidth = 1; //var, let, const - переменные
+let second = 2;
+const numberpi = 3.14
+{
+console.log(second); //console.log - отображает данные в косоли
+}
+var number = 50;
+console.log(56/2);
+var string = "Hello!";
+var obj = {};  //объект пишется в скобках {}
+
+
+let persone = {
+    name: "Chris",
+    age: 16,
+    information: "He likes to play guitar and dota. He also goes to gym.",
+    hasagirlfriend: false,
+
+};
+console.log(persone.name);
+console.log(persone ["information"]); //чтобы вытащиь данные из обьекта, надо записать его имя в [] и ""
+let images = ['banana.jpg', 'apple.png'];
+console.log(images[0]); //выводит в консоль переменную или предмет по номеру 
+// alert('Are you sure?'); // alert - окно с вопросом 
+
+//let answer = confirm("Are you here?"); окно с вопросом и строкой для введения ответа
+let answer = prompt("Are u 18 y.o.?" , "Yes");
+console.log(typeof(answer));
+
+let incr = 25,
+    decr = 25;
+
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(15%4);
+    console.log("24" == 24);
+
+    let isChecked = true,
+        isClose = false;
+
+        console.log(isChecked && !isClose);
+        console.log(isChecked || isClose);
+
+
